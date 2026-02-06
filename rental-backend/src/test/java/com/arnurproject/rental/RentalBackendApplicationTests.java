696b8da057014c5a5dcf2b66582fce4b5e26be44
Axxelium <59525687+Axxelium@users.noreply.github.com>
2026-02-06 13:40:53 +0500

    Add BACKEND assamblage
    Добавление сборки проекта созданного в start.spring.io

---
+++ rental-backend/src/test/java/com/arnurproject/rental/RentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnurproject.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
